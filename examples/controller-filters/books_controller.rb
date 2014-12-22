573b55043c0e90eb77b0eead2684264531769746
Manfred Stienstra <manfred@fngtps.com>
2014-12-22 22:59:31 +0100

    Add a draft of a first article.

--- examples/controller-filters/books_controller.rb
+++ examples/controller-filters/books_controller.rb
@@ -1,8 +1,14 @@
 class BooksController < ApplicationController
+  # Using a filter allows us to set the @authenticated
+  # instance variable for every request and make sure
+  # someone is signed in. It reduces the branching in
+  # the actions and makes them overall shorter.
+  before_action :find_authenticated
+
   # We've replaced the repeated Book.find from the actions
   # with a before filter. This allows us to keep the
   # contents of the actions shorter and more readable.
-  before_filter :find_book, only: %i(show edit update destroy)
+  before_action :find_book, only: %i(show edit update destroy)
 
   def new
     @book = Book.new
@@ -34,10 +40,38 @@ class BooksController < ApplicationController
 
   private
 
+  # We read the authenticated through a controller method and
+  # in the views we read it through a helper method. This allows
+  # us to override in subclasses or specific views.
+  attr_reader :authenticated
+  helper_method :authenticated
+
+  # By wrapping the authentication token in a method we open
+  # up to possibility for other ways to present this token.
+  # For example through params, the session, or a header.
+  def authentication_token
+    cookies[:authentication_token]
+  end
+
+  # This is the only place where we now have the branch to
+  # see if anyone is signed in.
+  def find_authenticated
+    if @session = Session.find_by(token: authentication_token)
+      @authenticated = @session.target
+    else
+      head :unauthorized
+    end
+  end
+
+  # We use Book.find so Rails throws and catches the RecordNotFound
+  # error for us and takes care of a nice 404 page for non-existent
+  # books.
   def find_book
     @book = Book.find(params[:id])
   end
 
+  # We read the book params from an accessor so we only need to
+  # change one method when a book record needs to accept more attributes.
   def book_params
     params.require(:book).permit(:title, :description)
   end
--- examples/controller-filters/unabridged_books_controller.rb
+++ examples/controller-filters/unabridged_books_controller.rb
@@ -1,47 +1,81 @@
 class UnabridgedBooksController < ApplicationController
   def new
-    @book = Book.new
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+      @book = Book.new
+    else
+      head :unauthorized
+    end
   end
 
   def create
-    # We repeat the params permit line for the create and
-    # update actions. This seems like a pattern.
-    @book = Book.new(params.require(:book).permit(
-      :title, :description
-    ))
-    if @book.save
-      redirect_to @book
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+
+      # We repeat the params permit line for the create and
+      # update actions. This seems like a pattern.
+      @book = Book.new(params.require(:book).permit(
+        :title, :description
+      ))
+      if @book.save
+        redirect_to @book
+      else
+        render :new
+      end
     else
-      render :new
+      head :unauthorized
     end
   end
 
   def show
-    # We repeat the find on the book for the show, edit,
-    # update, and destroy action. This seems like a
-    # pattern.
-    @book = Boom.find(params[:id])
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+
+      # We repeat the find on the book for the show, edit,
+      # update, and destroy action. This seems like a
+      # pattern.
+      @book = Boom.find(params[:id])
+    else
+      head :unauthorized
+    end
   end
 
   def edit
-    @book = Boom.find(params[:id])
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+      @book = Boom.find(params[:id])
+    else
+      head :unauthorized
+    end
   end
 
   def update
-    @book = Boom.find(params[:id])
-    @book.attributes = params.require(:book).permit(
-      :title, :description
-    )
-    if @book.save
-      redirect_to @book
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+
+      @book = Boom.find(params[:id])
+      @book.attributes = params.require(:book).permit(
+        :title, :description
+      )
+      if @book.save
+        redirect_to @book
+      else
+        render :edit
+      end
     else
-      render :edit
+      head :unauthorized
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_url
+    if @session = Session.find_by(token: cookies[:authentication_token])
+      @authenticated = @session.target
+
+      @book = Book.find(params[:id])
+      @book.destroy
+      redirect_to books_url
+    else
+      head :unauthorized
+    end
   end
 end
